71abc132a868c7d11ee4bf1ea342005ef27ffa09
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-01-30 13:28:35 -0500

    Split out offline package

--- scripts/build_debs.sh
+++ scripts/build_debs.sh
@@ -33,12 +33,7 @@ docker run \
   kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
   kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
   kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
-  kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl \
-  images/registry.tar=/opt/images/registry.tar \
-  images/kubedns.tar=/opt/images/kubedns.tar \
-  images/kube-dnsmasq.tar=/opt/images/kube-dnsmasq.tar \
-  images/kubernetes-dashboard.tar=/opt/images/kubernetes-dashboard.tar \
-  images/exechealthz.tar=/opt/images/exechealthz.tar
+  kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl
 
 # worker
 docker run \
@@ -66,13 +61,41 @@ docker run \
   --description "Kubernetes node binaries" \
   --url "https://apprenda.com/kismatic" \
   kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
+  kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy
+
+# offline
+docker run \
+  -v $(pwd)/source/:/source/ \
+  -v $(pwd)/build/:/build/ \
+  kismatic/fpm fpm \
+  -s dir \
+  -n "kismatic-offline" \
+  -v $KISMATIC_PACKAGE_VERSION  \
+  -a amd64 \
+  -t deb \
+  -d "kismatic-docker-engine = $DOCKER_DEB_VERSION_SHORT" \
+  -p /build/debs \
+  -C /source/ \
+  --license "Apache Software License 2.0" \
+  --maintainer "Apprenda <info@apprenda.com>" \
+  --vendor "Apprenda" \
+  --description "Kubernetes node binaries" \
+  --url "https://apprenda.com/kismatic" \
+  kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
   kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
+  images/registry.tar=/opt/images/registry.tar \
+  images/kubedns.tar=/opt/images/kubedns.tar \
+  images/kube-dnsmasq.tar=/opt/images/kube-dnsmasq.tar \
+  images/kubernetes-dashboard.tar=/opt/images/kubernetes-dashboard.tar \
+  images/exechealthz.tar=/opt/images/exechealthz.tar \
   images/pause.tar=/opt/images/pause.tar \
   images/nginx.tar=/opt/images/nginx.tar \
   images/busybox.tar=/opt/images/busybox.tar \
   images/defaultbackend.tar=/opt/images/defaultbackend.tar \
   images/nginx-ingress-controller.tar=/opt/images/nginx-ingress-controller.tar \
-  images/tcp-healthz-amd64.tar=/opt/images/tcp-healthz-amd64.tar
+  images/tcp-healthz-amd64.tar=/opt/images/tcp-healthz-amd64.tar \
+  images/calico.tar=/opt/images/calico.tar \
+  images/kube-policy-controller.tar=/opt/images/kube-policy-controller.tar
 
 # networking
 docker run \
@@ -92,9 +115,7 @@ docker run \
   --description "Kubernetes networking binaries" \
   --url "https://apprenda.com/kismatic" \
   networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
-  networking/cni/bin/=/opt/cni/ \
-  images/calico.tar=/opt/images/calico.tar \
-  images/kube-policy-controller.tar=/opt/images/kube-policy-controller.tar
+  networking/cni/bin/=/opt/cni/
 
 # build etcd
 docker run \
--- scripts/sign_rpms.sh
+++ scripts/sign_rpms.sh
@@ -5,4 +5,10 @@ docker run \
   -v $(pwd)/build/rpms:/rpms/ \
   -v $KEYS_PATH:/keys/ \
   kismatic/fpm \
-  /bin/bash -c "gpg --import /keys/public.key gpg --import /keys/private.key; cd /root/ && ./rpm-sign.exp /rpms/*.rpm"
+  /bin/bash -c "gpg --import /keys/public.key && gpg --import /keys/private.key; cd /root/ \
+  && ./rpm-sign.exp /rpms/kismatic-docker-engine*.rpm \
+  && ./rpm-sign.exp /rpms/kismatic-etcd*.rpm \
+  && ./rpm-sign.exp /rpms/kismatic-kubernetes-master*.rpm \
+  && ./rpm-sign.exp /rpms/kismatic-kubernetes-networking*.rpm \
+  && ./rpm-sign.exp /rpms/kismatic-kubernetes-node*.rpm \
+  && ./rpm-sign.exp /rpms/kismatic-offline*.rpm"
