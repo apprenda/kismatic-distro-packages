58fe3b11f1fd187b0cbc3e8b0311ea0c95ebfad2
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-01-31 12:49:39 -0500

    Revert old method of signing RPMs

--- scripts/sign_rpms.sh
+++ scripts/sign_rpms.sh
@@ -5,10 +5,4 @@ docker run \
   -v $(pwd)/build/rpms:/rpms/ \
   -v $KEYS_PATH:/keys/ \
   kismatic/fpm \
-  /bin/bash -c "gpg --import /keys/public.key && gpg --import /keys/private.key; cd /root/ \
-  && ./rpm-sign.exp /rpms/kismatic-docker-engine*.rpm \
-  && ./rpm-sign.exp /rpms/kismatic-etcd*.rpm \
-  && ./rpm-sign.exp /rpms/kismatic-kubernetes-master*.rpm \
-  && ./rpm-sign.exp /rpms/kismatic-kubernetes-networking*.rpm \
-  && ./rpm-sign.exp /rpms/kismatic-kubernetes-node*.rpm \
-  && ./rpm-sign.exp /rpms/kismatic-offline*.rpm"
+  /bin/bash -c "gpg --import /keys/public.key gpg --import /keys/private.key; cd /root/ && ./rpm-sign.exp /rpms/*.rpm"
