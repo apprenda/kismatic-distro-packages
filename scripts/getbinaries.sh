d50b3d0fb43119e885e22982d657c48b565fa2d9
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-02-03 17:57:52 -0500

    Download etcd only once

--- scripts/getbinaries.sh
+++ scripts/getbinaries.sh
@@ -13,11 +13,9 @@ chmod 750 source/kubernetes/*/bin/*
 rm -rf source/etcd/
 mkdir -p source/etcd/k8s/bin/
 mkdir -p source/etcd/networking/bin/
-wget -P source/etcd/k8s/ $ETCD_URL && tar xvzf source/etcd/k8s/etcd-v3* -C source/etcd/k8s/ && rm source/etcd/k8s/etcd-v3*.tar.gz
-mv source/etcd/k8s/etcd-v3*/etcd source/etcd/k8s/bin/etcd_k8s
-mv source/etcd/k8s/etcd-v3*/etcdctl source/etcd/k8s/bin/etcdctl
-rm -rf source/etcd/k8s/etcd-v3*
-wget -P source/etcd/networking/ $ETCD_URL && tar xvzf source/etcd/networking/etcd-v3* -C source/etcd/networking && rm source/etcd/networking/etcd-v3*.tar.gz
-mv source/etcd/networking/etcd-v3*/etcd source/etcd/networking/bin/etcd_networking
-rm -rf source/etcd/networking/etcd-v3*
+wget -P source/etcd/ $ETCD_URL && tar xvzf source/etcd/etcd-v3* -C source/etcd/ && rm source/etcd/etcd-v3*.tar.gz
+cp source/etcd/etcd-v3*/etcd source/etcd/networking/bin/etcd_networking
+mv source/etcd/etcd-v3*/etcd source/etcd/k8s/bin/etcd_k8s
+mv source/etcd/etcd-v3*/etcdctl source/etcd/k8s/bin/etcdctl
+rm -rf source/etcd/etcd-v3*
 chmod 750 source/etcd/*/bin/*
