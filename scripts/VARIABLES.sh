2443ecdb4ed7d082fd002b26e99efa48a23fde3e
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-02-03 14:46:57 -0500

    Packages to support static pods

--- scripts/VARIABLES.sh
+++ scripts/VARIABLES.sh
@@ -1,40 +1,27 @@
 #!/bin/bash
 
 # versions
-KISMATIC_PACKAGE_VERSION=1.5.2-3
-REQUIRED_RPM_KISMATIC_PACKAGE_VERSION=1.5.2_3-1
-REQUIRED_DEB_KISMATIC_PACKAGE_VERSION=1.5.2-3
+KISMATIC_PACKAGE_VERSION=1.5.2-4
 K8S_VERSION=1.5.2
-DOCKER_VERSION=1.11.2
-CALICO_CTL_VERSION=v1.0.0
-CALICO_CONTAINER_VERSION=v1.0.0
-CALICO_CNI_VERSION=v1.5.5
-CNI_VERSION=v0.3.0
-DOCKER_RPM_VERSION=1.11.2-1.el7.centos.x86_64
-DOCKER_SELINUX_RPM_VERSION=1.11.2-1.el7.centos.noarch
-DOCKER_DEB_VERSION=1.11.2-0~xenial_amd64
-DOCKER_DEB_VERSION_SHORT=1.11.2-0~xenial
-ETCD_VERSION=v3.0.15
-ETCD2_VERSION=v2.3.7
+ETCD_VERSION=v3.1.0
 
 K8S_URL=https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/bin/linux/amd64
-
-CNI_CALICO_CTL_URL=https://github.com/projectcalico/calico-containers/releases/download/$CALICO_CTL_VERSION/calicoctl
-CNI_CALICO_CNI_URL=https://github.com/projectcalico/calico-cni/releases/download/$CALICO_CNI_VERSION
-CNI_URL=https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-$CNI_VERSION.tgz
-
-DOCKER_RPM_URL=https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-$DOCKER_RPM_VERSION.rpm
-DOCKER_SELINUX_RPM_URL=https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-selinux-$DOCKER_SELINUX_RPM_VERSION.rpm
-DOCKER_DEB_URL=https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_$DOCKER_DEB_VERSION.deb
-
-ETCD_K8S_URL=https://github.com/coreos/etcd/releases/download/$ETCD_VERSION/etcd-$ETCD_VERSION-linux-amd64.tar.gz
-ETCD_NETWORKING_URL=https://github.com/coreos/etcd/releases/download/$ETCD2_VERSION/etcd-$ETCD2_VERSION-linux-amd64.tar.gz
+ETCD_URL=https://github.com/coreos/etcd/releases/download/$ETCD_VERSION/etcd-$ETCD_VERSION-linux-amd64.tar.gz
 
 
 # docker images
+# k8s control plance
+KUBE_PROXY_IMG=gcr.io/google-containers/kube-proxy-amd64:v$K8S_VERSION
+KUBE_CONTROLLER_MANAGER_IMG=gcr.io/google-containers/kube-controller-manager-amd64:v$K8S_VERSION
+KUBE_SCHEDULER_IMG=gcr.io/google-containers/kube-scheduler-amd64:v$K8S_VERSION
+KUBE_APISERVER_IMG=gcr.io/google-containers/kube-apiserver-amd64:v$K8S_VERSION
+# networking
+CALICO_IMG=calico/node:v1.0.2
+CALICO_CTL_IMG=calico/ctl:v1.0.2
+CALICO_CNI_IMG=calico/cni:v1.5.6
+CALICO_KUBE_POLICY_CONTROLLER_IMG=calico/kube-policy-controller:v0.5.2
+# install support
 REGISTRY_IMG=registry:2.5.1
-CALICO_IMG=calico/node:$CALICO_CONTAINER_VERSION
-CALICO_KUBE_POLICY_CONTROLLER_IMG=calico/kube-policy-controller:v0.4.0
 KUBEDNS_IMG=gcr.io/google_containers/kubedns-amd64:1.9
 DNSMAQ_IMG=gcr.io/google_containers/kube-dnsmasq-amd64:1.4
 EXECHEALTHZ_IMG=gcr.io/google_containers/exechealthz-amd64:1.2
--- scripts/build_rpms.sh
+++ scripts/build_rpms.sh
@@ -3,118 +3,6 @@ set -e
 
 source ./scripts/VARIABLES.sh
 
-# build Kubernetes
-# master
-docker run \
-  -v $(pwd)/source/:/source/ \
-  -v $(pwd)/build/:/build/ \
-  kismatic/fpm fpm \
-  -s dir\
-  -n 'kismatic-kubernetes-master' \
-  -v $KISMATIC_PACKAGE_VERSION \
-  -t rpm --rpm-os linux \
-  -a x86_64 \
-  -d "kismatic-kubernetes-node = $REQUIRED_RPM_KISMATIC_PACKAGE_VERSION" \
-  -d "kismatic-kubernetes-networking = $REQUIRED_RPM_KISMATIC_PACKAGE_VERSION" \
-  -d "kismatic-docker-engine = $DOCKER_VERSION" \
-  -d 'bridge-utils' \
-  -d 'iptables >= 1.4.21' \
-  -d 'socat' \
-  -d 'util-linux' \
-  -d 'ethtool' \
-  -d 'nfs-utils' \
-  -p /build/rpms/ \
-  -C /source/ \
-  --license "Apache Software License 2.0" \
-  --maintainer "Apprenda <info@apprenda.com>" \
-  --vendor "Apprenda" \
-  --description "Kubernetes master binaries" \
-  --url "https://apprenda.com/kismatic" \
-  kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
-  kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
-  kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
-  kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl
-
-# worker
-docker run \
-  -v $(pwd)/source/:/source/ \
-  -v $(pwd)/build/:/build/ \
-  kismatic/fpm fpm \
-  -s dir \
-  -n 'kismatic-kubernetes-node' \
-  -v $KISMATIC_PACKAGE_VERSION \
-  -t rpm --rpm-os linux \
-  -a x86_64 \
-  -d "kismatic-kubernetes-networking = $REQUIRED_RPM_KISMATIC_PACKAGE_VERSION" \
-  -d "kismatic-docker-engine = $DOCKER_VERSION" \
-  -d 'bridge-utils' \
-  -d 'iptables >= 1.4.21' \
-  -d 'socat' \
-  -d 'util-linux' \
-  -d 'ethtool' \
-  -d 'nfs-utils' \
-  -p /build/rpms/ \
-  -C /source/ \
-  --license "Apache Software License 2.0" \
-  --maintainer "Apprenda <info@apprenda.com>" \
-  --vendor "Apprenda" \
-  --description "Kubernetes node binaries" \
-  --url "https://apprenda.com/kismatic" \
-  kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
-  kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy
-
-# offline
-docker run \
-  -v $(pwd)/source/:/source/ \
-  -v $(pwd)/build/:/build/ \
-  kismatic/fpm fpm \
-  -s dir \
-  -n 'kismatic-offline' \
-  -v $KISMATIC_PACKAGE_VERSION \
-  -t rpm --rpm-os linux \
-  -a x86_64 \
-  -d "kismatic-docker-engine = $DOCKER_VERSION" \
-  -p /build/rpms/ \
-  -C /source/ \
-  --license "Apache Software License 2.0" \
-  --maintainer "Apprenda <info@apprenda.com>" \
-  --vendor "Apprenda" \
-  --description "Kubernetes node binaries" \
-  --url "https://apprenda.com/kismatic" \
-  images/registry.tar=/opt/images/registry.tar \
-  images/kubedns.tar=/opt/images/kubedns.tar \
-  images/kube-dnsmasq.tar=/opt/images/kube-dnsmasq.tar \
-  images/kubernetes-dashboard.tar=/opt/images/kubernetes-dashboard.tar \
-  images/exechealthz.tar=/opt/images/exechealthz.tar \
-  images/pause.tar=/opt/images/pause.tar \
-  images/nginx.tar=/opt/images/nginx.tar \
-  images/busybox.tar=/opt/images/busybox.tar \
-  images/defaultbackend.tar=/opt/images/defaultbackend.tar \
-  images/nginx-ingress-controller.tar=/opt/images/nginx-ingress-controller.tar \
-  images/tcp-healthz-amd64.tar=/opt/images/tcp-healthz-amd64.tar \
-  images/calico.tar=/opt/images/calico.tar \
-  images/kube-policy-controller.tar=/opt/images/kube-policy-controller.tar
-
-# networking
-docker run \
-  -v $(pwd)/source/:/source/ \
-  -v $(pwd)/build/:/build/ \
-  kismatic/fpm fpm \
-  -s dir \
-  -n "kismatic-kubernetes-networking" \
-  -v $KISMATIC_PACKAGE_VERSION \
-  -t rpm --rpm-os linux \
-  -a x86_64 \
-  -p /build/rpms/ \
-  -C /source/ \
-  --license "Apache Software License 2.0" \
-  --maintainer "Apprenda <info@apprenda.com>" \
-  --vendor "Apprenda" \
-  --description "Kubernetes networking binaries" \
-  --url "https://apprenda.com/kismatic" \
-  networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
-  networking/cni/bin/=/opt/cni/
-
 # build etcd
 docker run \
   -v $(pwd)/source/:/source/ \
@@ -136,21 +24,235 @@ docker run \
   etcd/k8s/bin/etcdctl=/usr/bin/etcdctl \
   etcd/networking/bin/etcd_networking=/usr/bin/etcd_networking
 
-# build docker
+# kubelet
 docker run \
   -v $(pwd)/source/:/source/ \
   -v $(pwd)/build/:/build/ \
   kismatic/fpm fpm \
-  -s rpm \
-  -n "kismatic-docker-engine" \
-  -v $DOCKER_VERSION \
-  -a x86_64 \
+  -s dir \
+  -n 'kismatic-kubelet' \
+  -v $KISMATIC_PACKAGE_VERSION \
   -t rpm --rpm-os linux \
+  -a x86_64 \
+  -d 'bridge-utils' \
+  -d 'iptables >= 1.4.21' \
+  -d 'socat' \
+  -d 'util-linux' \
+  -d 'ethtool' \
+  -d 'nfs-utils' \
   -p /build/rpms/ \
+  -C /source/ \
   --license "Apache Software License 2.0" \
   --maintainer "Apprenda <info@apprenda.com>" \
   --vendor "Apprenda" \
-  --description "Docker and its dependencies" \
+  --description "Kubernetes kubelet binaries" \
   --url "https://apprenda.com/kismatic" \
-  /source/docker/rpm/docker-engine-selinux-$DOCKER_SELINUX_RPM_VERSION.rpm \
-  /source/docker/rpm/docker-engine-$DOCKER_RPM_VERSION.rpm
+  kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet
+
+# kubectl
+docker run \
+  -v $(pwd)/source/:/source/ \
+  -v $(pwd)/build/:/build/ \
+  kismatic/fpm fpm \
+  -s dir \
+  -n 'kismatic-kubectl' \
+  -v $KISMATIC_PACKAGE_VERSION \
+  -t rpm --rpm-os linux \
+  -a x86_64 \
+  -p /build/rpms/ \
+  -C /source/ \
+  --license "Apache Software License 2.0" \
+  --maintainer "Apprenda <info@apprenda.com>" \
+  --vendor "Apprenda" \
+  --description "Kubernetes kubectl binaries" \
+  --url "https://apprenda.com/kismatic" \
+  kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl
+
+# offline
+docker run \
+  -v $(pwd)/source/:/source/ \
+  -v $(pwd)/build/:/build/ \
+  kismatic/fpm fpm \
+  -s dir \
+  -n 'kismatic-offline' \
+  -v $KISMATIC_PACKAGE_VERSION \
+  -t rpm --rpm-os linux \
+  -a x86_64 \
+  -p /build/rpms/ \
+  -C /source/ \
+  --license "Apache Software License 2.0" \
+  --maintainer "Apprenda <info@apprenda.com>" \
+  --vendor "Apprenda" \
+  --description "Kubernetes node binaries" \
+  --url "https://apprenda.com/kismatic" \
+  images/registry.tar=/opt/images/registry.tar \
+  images/kubedns.tar=/opt/images/kubedns.tar \
+  images/kube-dnsmasq.tar=/opt/images/kube-dnsmasq.tar \
+  images/kubernetes-dashboard.tar=/opt/images/kubernetes-dashboard.tar \
+  images/exechealthz.tar=/opt/images/exechealthz.tar \
+  images/pause.tar=/opt/images/pause.tar \
+  images/nginx.tar=/opt/images/nginx.tar \
+  images/busybox.tar=/opt/images/busybox.tar \
+  images/defaultbackend.tar=/opt/images/defaultbackend.tar \
+  images/nginx-ingress-controller.tar=/opt/images/nginx-ingress-controller.tar \
+  images/tcp-healthz-amd64.tar=/opt/images/tcp-healthz-amd64.tar \
+  images/kube-proxy.tar=/opt/images/kube-proxy.tar \
+  images/kube-controller-manager.tar=/opt/images/kube-controller-manager.tar \
+  images/kube-scheduler.tar=/opt/images/kube-scheduler.tar \
+  images/kube-apiserver.tar=/opt/images/kube-apiserver.tar \
+  images/calico.tar=/opt/images/calico.tar \
+  images/calico-ctl.tar=/opt/images/calico-ctl.tar \
+  images/calico-cni.tar=/opt/images/calico-cni.tar \
+  images/kube-policy-controller.tar=/opt/images/kube-policy-controller.tar
+
+# # build Kubernetes
+# # master
+# docker run \
+#   -v $(pwd)/source/:/source/ \
+#   -v $(pwd)/build/:/build/ \
+#   kismatic/fpm fpm \
+#   -s dir\
+#   -n 'kismatic-kubernetes-master' \
+#   -v $KISMATIC_PACKAGE_VERSION \
+#   -t rpm --rpm-os linux \
+#   -a x86_64 \
+#   -d "kismatic-kubernetes-node = $REQUIRED_RPM_KISMATIC_PACKAGE_VERSION" \
+#   -d "kismatic-kubernetes-networking = $REQUIRED_RPM_KISMATIC_PACKAGE_VERSION" \
+#   -d "kismatic-docker-engine = $DOCKER_VERSION" \
+#   -d 'bridge-utils' \
+#   -d 'iptables >= 1.4.21' \
+#   -d 'socat' \
+#   -d 'util-linux' \
+#   -d 'ethtool' \
+#   -d 'nfs-utils' \
+#   -p /build/rpms/ \
+#   -C /source/ \
+#   --license "Apache Software License 2.0" \
+#   --maintainer "Apprenda <info@apprenda.com>" \
+#   --vendor "Apprenda" \
+#   --description "Kubernetes master binaries" \
+#   --url "https://apprenda.com/kismatic" \
+#   kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
+#   kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
+#   kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
+#   kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl
+#
+# # worker
+# docker run \
+#   -v $(pwd)/source/:/source/ \
+#   -v $(pwd)/build/:/build/ \
+#   kismatic/fpm fpm \
+#   -s dir \
+#   -n 'kismatic-kubernetes-node' \
+#   -v $KISMATIC_PACKAGE_VERSION \
+#   -t rpm --rpm-os linux \
+#   -a x86_64 \
+#   -d "kismatic-kubernetes-networking = $REQUIRED_RPM_KISMATIC_PACKAGE_VERSION" \
+#   -d "kismatic-docker-engine = $DOCKER_VERSION" \
+#   -d 'bridge-utils' \
+#   -d 'iptables >= 1.4.21' \
+#   -d 'socat' \
+#   -d 'util-linux' \
+#   -d 'ethtool' \
+#   -d 'nfs-utils' \
+#   -p /build/rpms/ \
+#   -C /source/ \
+#   --license "Apache Software License 2.0" \
+#   --maintainer "Apprenda <info@apprenda.com>" \
+#   --vendor "Apprenda" \
+#   --description "Kubernetes node binaries" \
+#   --url "https://apprenda.com/kismatic" \
+#   kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
+#   kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy
+#
+# # offline
+# docker run \
+#   -v $(pwd)/source/:/source/ \
+#   -v $(pwd)/build/:/build/ \
+#   kismatic/fpm fpm \
+#   -s dir \
+#   -n 'kismatic-offline' \
+#   -v $KISMATIC_PACKAGE_VERSION \
+#   -t rpm --rpm-os linux \
+#   -a x86_64 \
+#   -d "kismatic-docker-engine = $DOCKER_VERSION" \
+#   -p /build/rpms/ \
+#   -C /source/ \
+#   --license "Apache Software License 2.0" \
+#   --maintainer "Apprenda <info@apprenda.com>" \
+#   --vendor "Apprenda" \
+#   --description "Kubernetes node binaries" \
+#   --url "https://apprenda.com/kismatic" \
+#   images/registry.tar=/opt/images/registry.tar \
+#   images/kubedns.tar=/opt/images/kubedns.tar \
+#   images/kube-dnsmasq.tar=/opt/images/kube-dnsmasq.tar \
+#   images/kubernetes-dashboard.tar=/opt/images/kubernetes-dashboard.tar \
+#   images/exechealthz.tar=/opt/images/exechealthz.tar \
+#   images/pause.tar=/opt/images/pause.tar \
+#   images/nginx.tar=/opt/images/nginx.tar \
+#   images/busybox.tar=/opt/images/busybox.tar \
+#   images/defaultbackend.tar=/opt/images/defaultbackend.tar \
+#   images/nginx-ingress-controller.tar=/opt/images/nginx-ingress-controller.tar \
+#   images/tcp-healthz-amd64.tar=/opt/images/tcp-healthz-amd64.tar \
+#   images/calico.tar=/opt/images/calico.tar \
+#   images/kube-policy-controller.tar=/opt/images/kube-policy-controller.tar
+#
+# # networking
+# docker run \
+#   -v $(pwd)/source/:/source/ \
+#   -v $(pwd)/build/:/build/ \
+#   kismatic/fpm fpm \
+#   -s dir \
+#   -n "kismatic-kubernetes-networking" \
+#   -v $KISMATIC_PACKAGE_VERSION \
+#   -t rpm --rpm-os linux \
+#   -a x86_64 \
+#   -p /build/rpms/ \
+#   -C /source/ \
+#   --license "Apache Software License 2.0" \
+#   --maintainer "Apprenda <info@apprenda.com>" \
+#   --vendor "Apprenda" \
+#   --description "Kubernetes networking binaries" \
+#   --url "https://apprenda.com/kismatic" \
+#   networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
+#   networking/cni/bin/=/opt/cni/
+#
+# # build etcd
+# docker run \
+#   -v $(pwd)/source/:/source/ \
+#   -v $(pwd)/build/:/build/ \
+#   kismatic/fpm fpm \
+#   -s dir \
+#   -n "kismatic-etcd" \
+#   -v $KISMATIC_PACKAGE_VERSION \
+#   -a x86_64 \
+#   -t rpm --rpm-os linux \
+#   -p /build/rpms/ \
+#   -C /source/ \
+#   --license "Apache Software License 2.0" \
+#   --maintainer "Apprenda <info@apprenda.com>" \
+#   --vendor "Apprenda" \
+#   --description "Etcd kubernetes and networking binaries" \
+#   --url "https://apprenda.com/kismatic" \
+#   etcd/k8s/bin/etcd_k8s=/usr/bin/etcd_k8s \
+#   etcd/k8s/bin/etcdctl=/usr/bin/etcdctl \
+#   etcd/networking/bin/etcd_networking=/usr/bin/etcd_networking
+#
+# # build docker
+# docker run \
+#   -v $(pwd)/source/:/source/ \
+#   -v $(pwd)/build/:/build/ \
+#   kismatic/fpm fpm \
+#   -s rpm \
+#   -n "kismatic-docker-engine" \
+#   -v $DOCKER_VERSION \
+#   -a x86_64 \
+#   -t rpm --rpm-os linux \
+#   -p /build/rpms/ \
+#   --license "Apache Software License 2.0" \
+#   --maintainer "Apprenda <info@apprenda.com>" \
+#   --vendor "Apprenda" \
+#   --description "Docker and its dependencies" \
+#   --url "https://apprenda.com/kismatic" \
+#   /source/docker/rpm/docker-engine-selinux-$DOCKER_SELINUX_RPM_VERSION.rpm \
+#   /source/docker/rpm/docker-engine-$DOCKER_RPM_VERSION.rpm
