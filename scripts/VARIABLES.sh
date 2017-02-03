c4944bf81df05decaddab037c17f332e726e8bcd
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-02-03 17:18:27 -0500

    Remove old code

--- scripts/VARIABLES.sh
+++ scripts/VARIABLES.sh
@@ -5,9 +5,9 @@ KISMATIC_PACKAGE_VERSION=1.5.2-4
 K8S_VERSION=1.5.2
 ETCD_VERSION=v3.1.0
 
+# urls
 K8S_URL=https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/bin/linux/amd64
 ETCD_URL=https://github.com/coreos/etcd/releases/download/$ETCD_VERSION/etcd-$ETCD_VERSION-linux-amd64.tar.gz
-
 
 # docker images
 # k8s control plance
@@ -29,8 +29,8 @@ KUBERNETES_DASHBOARD_IMG=gcr.io/google_containers/kubernetes-dashboard-amd64:v1.
 DEFAULT_BACKEND=gcr.io/google_containers/defaultbackend:1.0
 NGINX_INGRESS_CONTROLLER=gcr.io/google_containers/nginx-ingress-controller:0.8.3
 APPRENDA_TCP_HEALTHZ=apprenda/tcp-healthz-amd64:v1.0.0
-# Used internally by k8s
+# used internally by k8s
 PAUSE_IMG=gcr.io/google_containers/pause-amd64:3.0
-# Used by kuberang
+# used by kuberang
 NGINX_IMG=nginx:stable-alpine
 BUSYBOX_IMG=busybox:latest
--- scripts/getbinaries.sh
+++ scripts/getbinaries.sh
@@ -6,27 +6,8 @@ source ./scripts/VARIABLES.sh
 # k8s
 rm -rf source/kubernetes
 wget -P source/kubernetes/kubelet/bin/ $K8S_URL/kubelet
-# wget -P source/kubernetes/proxy/bin/ $K8S_URL/kube-proxy
-# wget -P source/kubernetes/scheduler/bin/ $K8S_URL/kube-scheduler
-# wget -P source/kubernetes/apiserver/bin/ $K8S_URL/kube-apiserver
-# wget -P source/kubernetes/controller-manager/bin/ $K8S_URL/kube-controller-manager
 wget -P source/kubernetes/kubectl/bin/ $K8S_URL/kubectl
 chmod 750 source/kubernetes/*/bin/*
-
-# # cni
-# rm -rf source/networking/
-# wget -P source/networking/ctl/bin/ $CNI_CALICO_CTL_URL
-# chmod 770 source/networking/ctl/bin/*
-# wget -P source/networking/cni/bin/ $CNI_CALICO_CNI_URL/calico
-# wget -P source/networking/cni/bin/ $CNI_CALICO_CNI_URL/calico-ipam
-# wget -P source/networking/cni/ $CNI_URL && tar xvzf source/networking/cni/cni-* -C source/networking/cni/bin/ && rm source/networking/cni/cni-*.tgz
-# chmod 750 source/networking/cni/bin/*
-
-# # docker
-# rm -rf source/docker/
-# wget -P source/docker/rpm/ $DOCKER_RPM_URL
-# wget -P source/docker/rpm/ $DOCKER_SELINUX_RPM_URL
-# wget -P source/docker/deb/ $DOCKER_DEB_URL
 
 # etcd
 rm -rf source/etcd/
